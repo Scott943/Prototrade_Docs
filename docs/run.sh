0d9fb88192eeabbf895ec698212c2354b215e8a6
Scott Parker <scott.parker.uk@btinternet.com>
2023-02-01 18:53:09 +0000

    Update docs

--- docs/run.sh
+++ docs/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
- 
+for f in source/*.rst; do
+   # if [ f != "index.rst" ] || [ f != "modules.rst" ] 
+   # then
+   #    :
+   # fi
+
+  echo "File -> $f"
+done
+
 sphinx-apidoc -f -o source ../../3rd_year_project/prototrade/src/prototrade && make github && git add ../ && git commit -m "Update docs" && git push
