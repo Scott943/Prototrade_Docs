04dcd02f99bfa344ba4e384e2bb3f413fe153fa6
Scott943 <scott.parker.uk@btinternet.com>
2023-02-03 20:18:57 +0000

    Update docs

--- docs/run.sh
+++ docs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sphinx-apidoc -f -o source ../../3rd_year_project/prototrade/src/prototrade && 
+# sphinx-apidoc -f -o source ../../3rd_year_project/prototrade/src/prototrade && 
 make github && 
 git add ../ && 
 git commit -m "Update docs" && 
